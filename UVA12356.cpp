dc11b4153d9e98f88ebcff277856641ae1d70850
Nico Klaassen <klaassen.nico@gmail.com>
2019-02-16 20:00:37 +0100

    Updated solutions

---
+++ UVA12356.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int s, b;
+    while (cin >> s >> b) {
+        if (s == 0 && b == 0) break;
+
+        vector<int> soldiers;
+        for (int i = 0; i < s; i++) {
+            soldiers.push_back(1);
+        }
+
+        int left, right;
+        while (b--) {
+            cin >> left >> right;
+            
+        }
+    }
+}
