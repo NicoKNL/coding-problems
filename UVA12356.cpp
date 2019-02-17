ea6a1ac545f4bda3dc352eacda2b054a475dd99e
Nico Klaassen <klaassen.nico@gmail.com>
2019-02-17 18:30:59 +0100

    Updated solutions

--- UVA12356.cpp
+++ UVA12356.cpp
@@ -8,15 +8,37 @@ int main() {
     while (cin >> s >> b) {
         if (s == 0 && b == 0) break;
 
-        vector<int> soldiers;
-        for (int i = 0; i < s; i++) {
-            soldiers.push_back(1);
+        vector<vector<int>> soldiers;
+        int left, right;
+        for (int i = 0; i <= s + 1; i++) {
+            left = i - 1 < 1 ? 0 : i - 1;
+            right = i + 1 > s ? 0 : i + 1;
+            soldiers.push_back(vector<int> {left, right});
         }
 
-        int left, right;
+        vector<int> L, R, new_neighbours;
         while (b--) {
             cin >> left >> right;
-            
+            L = soldiers[left];
+            R = soldiers[right];
+
+            soldiers[L[0]][1] = R[1];
+            soldiers[R[1]][0] = L[0];
+            new_neighbours = vector<int> {L[0], R[1]};
+
+            for (int i = 0; i < 2; i++) {
+                if (new_neighbours[i] == 0) {
+                    cout << '*';
+                } else {
+                    cout << new_neighbours[i];
+                }
+                if (i == 0) {
+                    cout << " ";
+                } else {
+                    cout << endl;
+                }
+            }
         }
+        if (b) cout << "-" << endl;
     }
 }
