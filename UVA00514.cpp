532d1fc040c184c857aa9dae97f78eab4691865c
NicoKNL <klaassen.nico@gmail.com>
2019-02-26 16:11:29 +0100

    Updated solutions

--- UVA00514.cpp
+++ UVA00514.cpp
@@ -2,31 +2,42 @@
 #include <stack>
 
 using namespace std;
-// TODO: finish
+
 int main() {
-    bool reset = false;
+    bool reset = true;
+    int n;
+    int input;
     while (true) {
         stack<int> s;
-        int n;
-        int input;
-        cin >> n;
-        if (n == 0) {
-            break;
-        }
 
         if (reset) {
-            cout << endl;
             reset = false;
+            cin >> n;
+            if (n == 0) {
+                break;
+            }
         }
+
         cin >> input;
+        if (input == 0) {
+            reset = true;
+            cout << endl;
+            continue;
+        }
+        int input_counter = 1;
         int counter = 1;
         while (counter <= n) {
             s.push(counter);
             while (!s.empty()) {
                 if (s.top() == input) {
                     s.pop();
-                    cin >> input;
-                    if (input == 0) reset = true;
+
+                    if (input_counter < n) {
+                        cin >> input;
+                        input_counter++;
+                    } else {
+                        break;
+                    }
                 } else {
                     break;
                 }
@@ -34,6 +45,10 @@ int main() {
             counter++;
         }
 
+        while (input_counter < n) {
+            cin >> input;
+            input_counter++;
+        }
 
         if (s.empty()) {
             cout << "Yes" << endl;
