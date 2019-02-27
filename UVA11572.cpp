b6b1d78c01f8c5185bc4ed27cac219cde1690b5d
Nico <klaassen.nico@gmail.com>
2019-02-27 23:02:10 +0100

    Updated solutions

--- UVA11572.cpp
+++ UVA11572.cpp
@@ -2,16 +2,27 @@
 #include <map>
 
 using namespace std;
-
+// TODO: test through uDebug
 int main() {
-    int n, flakes, f, length;
+    int n, flakes, f, length, maxlength;
     cin >> n;
     while (n--) {
+        map<int, int> m;
         cin >> flakes;
         length = 0;
         for (int i = 0; i < flakes; i++) {
             cin >> f;
+            if (m.find(f) == m.end()) {
+                length++;
+                m[f] = 1;
+            } else {
+                if (length > maxlength) {
+                    maxlength = length;
+                }
+                length--;
+            }
         }
+        cout << maxlength << endl;
     }
     return 0;
 }
