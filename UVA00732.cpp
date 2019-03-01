a2127fa3380a7a242eca9b8345456e683ec66407
NicoKNL <klaassen.nico@gmail.com>
2019-03-01 23:13:40 +0100

    Updated solutions

--- UVA00732.cpp
+++ UVA00732.cpp
@@ -1,12 +1,69 @@
 #include <iostream>
 #include <stack>
-#include <vector>
 #include <string>
+#include <vector>
 #include <algorithm>
 
-using namespace std;
+// TODO: cleanup file
 
-void show(string s) {
+using namespace std;
+vector<string> validSequences;
+vector<string> possibleSequences;
+string source, target;
+
+void stackSim2(const string& s, const string& t, int i, int j, stack<char> stk, string seq) {
+    stack<char> stk2 = stk;
+    if (i < s.length()) {
+        stk2.push(s.at(i));
+        stackSim2(s, t, i + 1, j, stk2, seq + "i");
+    }
+
+    if (j < i) {
+        if (t.at(j) == stk.top()) {
+            stk.pop();
+            stackSim2(s, t, i, j + 1, stk, seq + "o");
+        }
+    }
+
+    if (i == j && i == s.length()) {
+        validSequences.push_back(seq);
+    }
+}
+
+bool matches(string a, string b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    return a == b;
+}
+
+bool stackSim(string s, string t, string seq) {
+    stack<char> stk;
+    int i = 0;
+    int j = 0;
+    for (char c: seq) {
+        if (c == 'i') {
+            stk.push(s.at(i));
+            i++;
+        }
+
+        if (c == 'o') {
+            if (stk.empty()) {
+                return false;
+            } else {
+                c = stk.top();
+                if (c != t.at(j)) {
+                    return false;
+                }
+
+                stk.pop();
+                j++;
+            }
+        }
+    }
+    return true;
+}
+
+void output(string s) {
     for (int i = 0; i < s.length(); i++) {
         cout << s.at(i);
         if (i < s.length() - 1) {
@@ -16,35 +73,43 @@ void show(string s) {
     cout << endl;
 }
 
-void dfsStackSim(stack<char> stk, string s, string t, string current) {
-    if (s.length() > 0) { // i
-        return dfsStackSim(stk, s, t, current);
+void allPossibleSequences(string seq, int i, int j, int limit) {
+    if (!stackSim(source, target, seq)) return;
+
+    if (i == limit && j == limit) {
+        possibleSequences.push_back(seq);
     }
-    if (t.length() > 0) { // o
-        if (!stk.empty()) {
-            stk.pop();
-        }
+
+    if (i < limit) {
+        allPossibleSequences(seq + "i", i + 1, j, limit);
+    }
+
+    if (j < i) {
+        allPossibleSequences(seq + "o", i, j + 1, limit);
     }
 }
 
-// o o o i i i
-// o i o i o i
-
 int main() {
-    string source, target;
-    vector<string> valid_ops;
-    stack<char> s;
     while (getline(cin, source)) {
         getline(cin, target);
-        valid_ops.clear();
-        cout << source << " >> " << target << endl;
 
+        validSequences.clear();
+        possibleSequences.clear();
 
+        if (matches(source, target)) {
+            string sequence;
+            allPossibleSequences("", 0, 0, source.length());
 
-        sort(valid_ops.begin(), valid_ops.end());
+            for (string seq: possibleSequences) {
+                if (stackSim(source, target, seq)) {
+                    validSequences.push_back(seq);
+                }
+            }
+        }
+
         cout << "[" << endl;
-        for (string op: valid_ops) {
-            show(op);
+        for (string seq: validSequences) {
+            output(seq);
         }
         cout << "]" << endl;
     }
