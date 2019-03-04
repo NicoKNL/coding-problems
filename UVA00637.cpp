6d8620ad9db93b21f866f2b9299dbcac9f28afea
NicoKNL <klaassen.nico@gmail.com>
2019-03-04 21:48:15 +0100

    Updated solutions

--- UVA00637.cpp
+++ UVA00637.cpp
@@ -21,17 +21,39 @@ int main() {
 
         int i = 0;
         int page_number = 1;
-        while (i < sheets && page_number < n) {
-            if (page_number < n) front[i][1] = page_number;
+        while (i < sheets && page_number <= n) {
+            if (page_number <= n) front[i][1] = page_number;
             page_number++;
-            if (page_number < n) back[i][0] = page_number;
+            if (page_number <= n) back[i][0] = page_number;
             page_number++;
+            i++;
         }
-        
+
+        i = sheets - 1;
+        while (i >= 0 && page_number <= n) {
+            if (page_number <= n) back[i][1] = page_number;
+            page_number++;
+            if (page_number <= n) front[i][0] = page_number;
+            page_number++;
+            i--;
+        }
+
         cout << "Printing order for " << n << " pages:" << endl;
         for (int i = 0; i < sheets; i++) {
-            cout << "Sheet " << i + 1 << ", front: " << front[i][0] << ", " << front[i][1] << endl;
-            cout << "Sheet " << i + 1 << ", back : " << back[i][0] << ", " << back[i][1] << endl;
+            if (front[i][0] > -1 || front[i][1] > -1) {
+                if (front[i][0] == -1) {
+                    cout << "Sheet " << i + 1 << ", front: " << "Blank" << ", " << front[i][1] << endl;
+                } else {
+                    cout << "Sheet " << i + 1 << ", front: " << front[i][0] << ", " << front[i][1] << endl;
+                }
+            }
+            if (back[i][0] > -1 || back[i][1] > -1) {
+                if (back[i][1] == -1) {
+                    cout << "Sheet " << i + 1 << ", back : " << back[i][0] << ", " << "Blank" << endl;
+                } else {
+                    cout << "Sheet " << i + 1 << ", back : " << back[i][0] << ", " << back[i][1] << endl;
+                }
+            }
         }
     }
     return 0;
