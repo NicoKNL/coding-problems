6144945d857230cef7c43acb8ea0b7e367f11f32
NicoKNL <klaassen.nico@gmail.com>
2019-03-07 23:01:36 +0100

    Updated solutions

--- UVA10895.cpp
+++ UVA10895.cpp
@@ -5,43 +5,53 @@ using namespace std;
 
 int main() {
     int m, n, r, tmp;
-    cin >> m >> n;
-    vector<vector<pair<int, int>>> adj;
-    vector<vector<pair<int, int>>> adj_t;
+    while (cin >> m >> n) {
+        vector<vector<pair<int, int>>> adj;
+        vector<vector<pair<int, int>>> adj_t;
 
-    for (int i = 0; i < m; i++) {
-        cin >> r;
-        vector<pair<int, int>> row;
-        for (int j = 0; j < r; j++) {
-            cin >> tmp;
-            row.push_back(pair<int, int> {tmp, 0});
+        for (int i = 0; i < m; i++) {
+            cin >> r;
+            vector<pair<int, int>> row;
+            for (int j = 0; j < r; j++) {
+                cin >> tmp;
+                row.push_back(pair<int, int> {tmp, 0});
+            }
+
+            for (int j = 0; j < r; j++) {
+                cin >> tmp;
+                row[j].second = tmp;
+            }
+
+            adj.push_back(row);
         }
 
-        for (int j = 0; j < r; j++) {
-            cin >> tmp;
-            row[j].second = tmp;
+        for (int i = 0; i < n; i++) {
+            adj_t.push_back(vector<pair<int, int>>{});
         }
 
-        adj.push_back(row);
-    }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < adj[i].size(); j++) {
+                adj_t[adj[i][j].first - 1].push_back(pair<int, int> {i + 1, adj[i][j].second});
+            }
+        }
 
-    for (int i = 0; i < n; i++) {
-        adj_t.push_back(vector<pair<int, int>>{});
-    }
-
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < adj[i].size(); j++) {
-            adj_t[adj[i][j].first].push_back(pair<int, int> {i, adj[i][j].second});
+        cout << n << " " << m << endl;
+        for (int i = 0; i < n; i++) {
+            cout << adj_t[i].size();
+            for (int j = 0; j < adj_t[i].size(); j++) {
+                cout << " " << adj_t[i][j].first;
+            }
+            cout << endl;
+            for (int j = 0; j < adj_t[i].size(); j++) {
+                if (j > 0) {
+                    cout << " " << adj_t[i][j].second;
+                } else {
+                    cout << adj_t[i][j].second;
+                }
+            }
+            cout << endl;
         }
     }
 
-    cout << n << " " << m << endl;
-    for (int i = 0; i < n; i++) {
-        cout << adj_t[i].size();
-        for (int j = 0; j < adj_t[i].size(); j++) {
-            cout << " " << adj_t[i][j].second;
-        }
-        cout << endl;
-    }
     return 0;
 }
