b070a3c937852c15992af06c6451f6c79e11a3e2
NicoKNL <klaassen.nico@gmail.com>
2019-03-15 23:54:22 +0100

    Updated solutions

--- UVA00556.cpp
+++ UVA00556.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
+#include <string>
 
 using namespace std;
 
@@ -26,16 +28,17 @@ vector<int> rotate(vector<int> dir, vector<vector<int>> rotation) {
     return new_dir;
 }
 
+bool in_bounds(vector<int> pos, int rows, int cols) {
+    return 0 <= pos[0] && pos[0] < cols && 0 <= pos[1] && pos[1] < rows;
+}
+
 int main() {
     int rows, cols, tmp;
+    string s_tmp;
     vector<vector<int>> maze;
     vector<vector<int>> book;
 
     vector<int> dir = {0, 1};
-    dir = rotate(dir, ML);
-    dir = rotate(dir, MR);
-    dir = rotate(dir, MR);
-    dir = rotate(dir, ML);
 
     while (cin >> rows >> cols) {
         if (rows == 0 && cols == 0) break;
@@ -44,8 +47,9 @@ int main() {
         for (int r = 0; r < rows; r++) {
             vector<int> maze_row;
             vector<int> book_row;
+            cin >> s_tmp;
             for (int c = 0; c < cols; c++) {
-                cin >> tmp;
+                tmp = s_tmp.at(c) - '0';
                 maze_row.push_back(tmp);
                 book_row.push_back(0);
             }
@@ -56,22 +60,40 @@ int main() {
         vector<int> pos {0, rows - 1};
         vector<int> dir {1, 0};
         vector<int> next_pos;
-        // Set count on this spot to 1?
 
         while (true) {
             // step
             // Rotate right check
             dir = rotate(dir, MR);
-            next_pos = {pos[0] + dir[0], pos[1] + dir[1]};
-            if (0 <= next_pos[0] && next_pos[0] < cols && 0 <= next_pos[1] && next_pos[1] < rows && maze[next_pos[1]][next_pos[0]] == 0) {
-
+            next_pos = {pos[0] + dir[0], pos[1] - dir[1]};
+            if (in_bounds(next_pos, rows, cols) && maze[next_pos[1]][next_pos[0]] == 0) {
+                book[next_pos[1]][next_pos[0]] += 1;
+                dir = rotate(dir, ML);
+                pos = next_pos;
+            } else {
+                dir = rotate(dir, ML);
+                dir = rotate(dir, ML);
+                continue;
             }
             if (pos[0] == 0 && pos[1] == rows - 1) break;
         }
 
         // Start counting
+        int counter[5] = {0};
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (maze[r][c] == 0) {
+                    counter[book[r][c]]++;
+                }
+            }
+        }
 
         // Output
+        for (int i = 0; i < 5; i++) {
+            cout << setw(3) << counter[i];
+            if (i < 4) cout << " ";
+        }
+        cout << endl;
     }
 
 
