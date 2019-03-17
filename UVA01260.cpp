9bea1baa75b56fb9974921397f6c486d76a82ca3
Nico <klaassen.nico@gmail.com>
2019-03-17 15:36:33 +0100

    Updated solutions

---
+++ UVA01260.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#define vi vector<int>
+using namespace std;
+
+int main() {
+    int n, t, sum, tmp, count;
+    vi sales;
+    cin >> n;
+    while (n--) {
+        cin >> t;
+        sum = 0;
+        sales.clear();
+        for (int i = 0; i < t; i++) {
+            cin >> tmp;
+            sales.push_back(tmp);
+            count = 0;
+            if (i > 0) {
+                for (int j = 0; j < i; j++) {
+                    if (sales[j] <= tmp) count++;
+                }
+            }
+            sum += count;
+        }
+        cout << sum << endl;
+    }
+    return 0;
+};
+
+// TODO: hand in
