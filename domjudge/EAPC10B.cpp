91f867e184daddbf38a971b409847760868bbc9c
Nico <klaassen.nico@gmail.com>
2019-09-28 00:19:40 +0200

    Updated solutions

--- domjudge/EAPC10B.cpp
+++ domjudge/EAPC10B.cpp
@@ -2,6 +2,77 @@
 //todo: finish
 using namespace std;
 
+bool evenDigits(int a, int b, int c, int d) {
+    vector<int> digits(10); // 0 - 9
+    // Single digits
+    digits[a%10]++;
+    digits[b%10]++;
+    digits[c%10]++;
+    digits[d%10]++;
+
+    // If double digits
+    if (a > 9) {
+        digits[a/10]++;
+    }
+    if (b > 9) {
+        digits[b/10]++;
+    }
+    if (c > 9) {
+        digits[c/10]++;
+    }
+    if (d > 9) {
+        digits[d/10]++;
+    }
+
+//    for (int i = 0; i < 10; i++) {
+//        cout << digits[i] << endl;
+//    }
+    bool even = true;
+    int base = -1;
+    for (int i = 0; i < 10; i++) {
+        if (digits[i] == 0) continue;
+        if (base == -1) base = digits[i];
+        if (digits[i] != base) even = false;
+    }
+    return even;
+}
+
+bool canSplit(int a, int b, int c, int d) {
+    int tmpA, tmpB;
+    for (int i = 1; i < 15; i++) {
+        tmpA = 0;
+        tmpB = 0;
+        if ((1 & i) > 0) {
+            tmpA += a;
+        } else {
+            tmpB += a;
+        }
+
+        if ((2 & i) > 0) {
+            tmpA += b;
+        } else {
+            tmpB += b;
+        }
+
+        if ((4 & i) > 0) {
+            tmpA += c;
+        } else {
+            tmpB += c;
+        }
+
+        if ((8 & i) > 0) {
+            tmpA += d;
+        } else {
+            tmpB += d;
+        }
+
+        if (tmpA == tmpB) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
     int n;
     cin >> n;
@@ -10,33 +81,13 @@ int main() {
         cin >> a >> b >> c;
         d = c % 100;
         c = c / 100;
-        vector<int> digits(10); // 0 - 9
-        digits[a/10]++;
-        digits[a%10]++;
-        digits[b/10]++;
-        digits[b%10]++;
-        digits[c/10]++;
-        digits[c%10]++;
-        digits[d/10]++;
-        digits[d%10]++;
-        bool split = false;
-        if ((a + b == c + d) || (a + c == b + d) || (a + d == b + c) ||
-        (a + b + c == d) || (a + b + d == c) || (a + c + d == b) || (b + c + d == a)) {
-            split = true;
-        }
 
-        bool even = true;
-        int base = -1;
-        for (int i = 0; i < 10; i++) {
-            if (digits[i] == 0) continue;
-            if (base == -1) base = digits[i];
-            if (digits[i] != base) even = false;
-        }
-
-        if (!even || !split) {
-            printf("no\n");
-        } else {
+        bool even = evenDigits(a, b, c, d);
+        bool split = canSplit(a, b, c, d);
+        if (even && split) {
             printf("yes\n");
+        } else {
+            printf("no\n");
         }
     }
     return 0;
--- domjudge/EAPC13E.cpp
+++ domjudge/EAPC13E.cpp
@@ -6,29 +6,41 @@ int main() {
     int n;
     cin >> n;
     while (n--) {
-        map<string, int> pub1;
-        map<string, int> pub2;
+        int strings;
+        cin >> strings;
         string s;
-        int c;
-        cin >> c;
-        for (int i = 0; i < c; i++) {
+        string tmp;
+        map<string, int> key1;
+        map<string, int> key2;
+        for (int i = 0; i < strings; i++) {
             cin >> s;
-            pub1[s] = i;
+            key1[s] = i;
         }
 
-        for (int i = 0; i < c; i++) {
+        for (int i = 0; i < strings; i++) {
             cin >> s;
-            pub2[s] = i;
+            key2[s] = i;
         }
 
-        vector<string> cipher;
-        for (int i = 0; i < c; i++) {
+        vector<string> cipher(strings);
+        for (int i = 0; i < strings; i++) {
             cin >> cipher[i];
         }
 
-        for (auto entry:pub1) {
-            // todo: swap here
+        vector<string> result(strings);
+        for (auto entry : key1) {
+            result[entry.second] = cipher[key2[entry.first]];
         }
+
+        for (int i = 0; i < strings; i++) {
+            cout << result[i];
+            if (i < strings - 1) {
+                cout << " ";
+            }
+        }
+        cout << endl;
+
+
     }
     return 0;
 }
