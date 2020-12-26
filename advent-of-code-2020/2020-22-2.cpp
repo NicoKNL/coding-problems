eb632472e9443c12600810d70ef57eed2d14b5f4
NicoKNL <klaassen.nico@gmail.com>
2020-12-26 17:34:07 +0100

    Updated solutions

--- advent-of-code-2020/2020-22-2.cpp
+++ advent-of-code-2020/2020-22-2.cpp
@@ -1,8 +1,89 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-list<int> p1;
-list<int> p2;
+list<int> player1;
+list<int> player2;
+
+int game = 0;
+
+list<int> getCopyUpTo(list<int> l, int index)
+{
+    list<int> result;
+    while (index--) {
+        result.push_back(l.front());
+        l.pop_front();
+    } 
+    return result;
+}
+
+__uint128_t hashDeck(list<int> & p1, list<int> & p2)
+{
+    __uint128_t h = 0;
+    for (int & i : p1) {
+        h += ((__uint128_t) 1 << (__uint128_t)(i - 1));
+    }
+
+    for (int & i : p2) {
+        h += ((__uint128_t) 1 << (__uint128_t)(i - 1 + 64));
+    }
+    return h;
+}
+
+bool play(list<int> & p1, list<int> & p2) // true => p1 wins
+{
+    ++game;
+    set<__uint128_t> hashes;
+    int round = 1;
+
+    while (!p1.empty() && !p2.empty()) {
+        __uint128_t current_hash = hashDeck(p1, p2);
+
+        if (hashes.find(current_hash) != hashes.end()) {
+            int top1 = p1.front(); p1.pop_front();
+            int top2 = p2.front(); p2.pop_front();
+
+            p1.push_back(top1);
+            p1.push_back(top2);
+            continue;
+        } else {
+            hashes.insert(current_hash);
+        }
+
+        int top1 = p1.front(); p1.pop_front();
+        int top2 = p2.front(); p2.pop_front();
+        bool winner;
+
+        if (top1 <= p1.size() && top2 <= p2.size()) {
+            list<int> sub1 = getCopyUpTo(p1, top1);
+            list<int> sub2 = getCopyUpTo(p2, top2);
+
+            winner = play(sub1, sub2);
+        } else {
+            if (top1 > top2) {
+                winner = true;
+            } else {
+                winner = false;
+            }
+        }
+
+        if (winner) {
+            p1.push_back(top1);
+            p1.push_back(top2);
+        } else {
+            p2.push_back(top2);
+            p2.push_back(top1);
+        }
+
+        ++round;
+    }
+    
+    --game;
+    if (p1.empty()) {
+        return false; // p2 wins
+    } else {
+        return true; // p1 wins
+    }
+}
 
 int main() 
 {
@@ -22,38 +103,29 @@ int main()
             player_1 = false;
         } else {
             if (player_1) {
-                p1.push_back(stoi(line));
+                player1.push_back(stoi(line));
             } else {
-                p2.push_back(stoi(line));
+                player2.push_back(stoi(line));
             }
         }
     }
 
-    while (!p1.empty() && !p2.empty()) {
-        int top1 = p1.front(); p1.pop_front();
-        int top2 = p2.front(); p2.pop_front();
-        if (top1 > top2) {
-            p1.push_back(top1);
-            p1.push_back(top2);
-        } else {
-            p2.push_back(top2);
-            p2.push_back(top1);
-        }
-    }
-
+    play(player1, player2);
     list<int> winner;
-    if (p1.empty()) {
-        winner = p2;
+
+    if (player1.empty()) {
+        winner = player2;
     } else {
-        winner = p1;
+        winner = player1;
     }
 
     long long result = 0;
-    int i = 1;
+    long long i = 1;
     while (!winner.empty()) {
-        result += i * winner.back();
+        result += i * (long long) winner.back();
         winner.pop_back();
         ++i;
     }
+
     cout << result << endl;
 }
