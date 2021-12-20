96bde7f4e1a97131e62072096ba7ff25c34fcd78
NicoKNL <klaassen.nico@gmail.com>
2021-12-20 20:21:49 +0100

    updated solutions

--- advent-of-code-2021/18-1.cpp
+++ advent-of-code-2021/18-1.cpp
@@ -2,33 +2,204 @@
 
 using namespace std;
 
-struct Node;
-struct Pair;
-struct Number;
+enum MODE
+{
+    EXPLODE,
+    SPLIT,
+};
 
 struct Node
 {
+    int value;
+    Node *parent;
+    Node *left;
+    Node *right;
+
+    Node()
+    {
+        value = -1;
+        parent = nullptr;
+        left = nullptr;
+        right = nullptr;
+    }
 };
 
-struct Pair : public Node
+Node *parse(string &s, Node *node)
 {
-};
+    char c = s[0];
+    s = s.substr(1);
 
-struct Number : public Node
+    if ('0' <= c && c <= '9')
+    {
+        cout << "c: " << c << endl;
+        node->value = c - '0';
+        cout << node->value << endl;
+        return node;
+    }
+
+    if (c == '[')
+    {
+        Node *left_node = new Node();
+        left_node->parent = node;
+        node->left = parse(s, left_node);
+    }
+
+    c = s[0];
+    s = s.substr(1);
+
+    if (c == ',')
+    {
+        Node *right_node = new Node();
+        right_node->parent = node;
+        node->right = parse(s, right_node);
+    }
+
+    c = s[0];
+    s = s.substr(1);
+
+    if (c == ']')
+    {
+        return node;
+    }
+
+    return node;
+}
+
+void dfs(Node *node)
 {
-};
+    if (node->value != -1)
+    {
+        cout << "value: " << node->value << endl;
+        return;
+    }
+
+    dfs(node->left);
+    dfs(node->right);
+}
+
+void addRight(Node *node, int value)
+{
+    if (node->value != -1)
+    {
+        node->value += value;
+        return;
+    }
+
+    addRight(node->right, value);
+}
+
+void upstreamLeft(Node *node, int value)
+{
+    if (node == nullptr)
+        return;
+
+    if (node == node->parent->right)
+    {
+        addRight(node->parent->left, value);
+        return;
+    }
+
+    upstreamLeft(node->parent, value);
+}
+
+void addLeft(Node *node, int value)
+{
+    if (node->value != -1)
+    {
+        node->value += value;
+        return;
+    }
+
+    addLeft(node->left, value);
+}
+
+void upstreamRight(Node *node, int value)
+{
+    if (node == nullptr)
+        return;
+
+    if (node == node->parent->left)
+    {
+        addLeft(node->parent->right, value);
+        return;
+    }
+
+    upstreamRight(node->parent, value);
+}
+
+bool explode(Node *node)
+{
+    queue<pair<int, Node *>> Q;
+    Q.push({0, node});
+
+    while (!Q.empty())
+    {
+        int depth;
+        Node *node;
+
+        tie(depth, node) = Q.front();
+        Q.pop();
+
+        if (node->value == -1)
+        {
+            Q.push({depth + 1, node->left});
+            Q.push({depth + 1, node->right});
+        }
+        else if (depth > 4)
+        {
+            int left_value = node->value;
+            int right_value = node->parent->right->value;
+            cout << "exploding: [" << left_value << "," << right_value << "]" << endl;
+            // upstreamLeft(node->parent, left_value);
+            upstreamRight(node->parent, right_value);
+
+            return true;
+        }
+    }
+
+    return false;
+}
 
 int main()
 {
     string line;
-
-    vector<Node *> V;
+    vector<Node *> nums;
     while (getline(cin, line))
     {
         if (line.length() == 0)
             break;
 
-        Node *node = parse(line);
+        cout << line << endl;
+        Node *node = new Node();
+        parse(line, node);
+        cout << line << endl;
+        nums.push_back(node);
     }
+
+    dfs(nums[0]);
+    explode(nums[0]);
+    dfs(nums[0]);
+    // for (int i = 1; i < nums.size(); ++i)
+    // {
+    //     nums[0] = "[" + nums[0] + "," + nums[i] + "]";
+    //     nums[0] = resolve(nums[0]);
+    // }
+
+    // MODE mode = EXPLODE;
+    // bool reduce = true;
+    // while (reduce)
+    // {
+    //     reduce = false;
+
+    //     switch (mode)
+    //     {
+    //     case EXPLODE:
+    //         line = explode(line);
+    //         break;
+    //     case SPLIT:
+    //         break;
+    //     }
+    // }
+
     return 0;
 }
---
+++ advent-of-code-2021/20-1.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define PADDING 100
+
+ostream &operator<<(ostream &os, const vector<string> &V)
+{
+    for (const string &s : V)
+    {
+        os << s << endl;
+    }
+    os << endl;
+    return os;
+}
+
+vector<string> createGrid()
+{
+    string base(3 * PADDING, '.');
+    vector<string> V(3 * PADDING, base);
+    return V;
+}
+
+int getIndex(string s)
+{
+    reverse(s.begin(), s.end());
+    int index = 0;
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (s[i] == '#')
+            index += (1 << i);
+    }
+    return index;
+}
+
+char applyMap(const vector<string> &V, const string &M, int r, int c)
+{
+    vector<pair<int, int>> offsets = {
+        {-1, -1},
+        {-1, 0},
+        {-1, 1},
+        {0, -1},
+        {0, 0},
+        {0, 1},
+        {1, -1},
+        {1, 0},
+        {1, 1}};
+
+    string code = "";
+    for (int i = 0; i < offsets.size(); ++i)
+    {
+        int ro = offsets[i].first;
+        int co = offsets[i].second;
+
+        code += V[r + ro][c + co];
+    }
+
+    int index = getIndex(code);
+    return M[index];
+}
+
+vector<string> step(const vector<string> &V, const string &M)
+{
+    vector<string> next_V = createGrid();
+
+    for (int r = 1; r < V.size() - 1; ++r)
+    {
+        for (int c = 1; c < V[0].size() - 1; ++c)
+        {
+            next_V[r][c] = applyMap(V, M, r, c);
+        }
+    }
+
+    return next_V;
+}
+
+int main()
+{
+    string line;
+    string M;
+    while (getline(cin, line))
+    {
+        if (line.length() == 0)
+            break;
+
+        M = line;
+    }
+
+    vector<string> V = createGrid();
+
+    int r = PADDING;
+    while (getline(cin, line))
+    {
+        if (line.length() == 0)
+            break;
+
+        for (int i = 0; i < line.length(); ++i)
+        {
+            V[r][PADDING + i] = line[i];
+        }
+        ++r;
+    }
+
+    cout << V << endl;
+    V = step(V, M);
+    V = step(V, M);
+    cout << V << endl;
+
+    int count = 0;
+    for (int r = 3; r < V.size() - 3; ++r)
+    {
+        for (int c = 3; c < V[0].size() - 3; ++c)
+        {
+            if (V[r][c] == '#')
+                ++count;
+        }
+    }
+    cout << count << endl;
+
+    return 0;
+}
