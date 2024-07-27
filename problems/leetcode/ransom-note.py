efb605c1d7ea2caed7593061a0af8a4382c2d676
Nico Klaassen <klaassen.nico@gmail.com>
2024-07-27 17:48:07 +0200

    Create ransom-note.py

---
+++ problems/leetcode/ransom-note.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        letters = defaultdict(int)
+
+        for letter in magazine:
+            letters[letter] += 1
+
+        for letter in ransomNote:
+            letters[letter] -= 1
+
+            if letters[letter] < 0:
+                return False
+
+        return True
