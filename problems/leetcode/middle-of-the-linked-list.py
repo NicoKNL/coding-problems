8248f5ffb43e2152de46956090dfdb86514d34a5
Nico Klaassen <klaassen.nico@gmail.com>
2024-07-27 17:47:36 +0200

    Create middle-of-the-linked-list.py

---
+++ problems/leetcode/middle-of-the-linked-list.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        count = 0
+
+        end = head
+        while end:
+            end = end.next
+            count += 1
+
+        middle = count // 2
+
+        target = head
+        for _ in range(middle):
+            target = target.next
+
+        return target
